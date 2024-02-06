64dd2056aba51326027fe89734f512713916cb16
Deva <devanand151101@gmail.com>
2024-02-06 11:44:22 +0530

    User details completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,37 @@ import { ApiService } from './services/api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
-  constructor(
-    private apiService: ApiService
-  ) {}
+export class AppComponent implements OnInit {
+  username: string = '';
+  page_no: number = 1;
+  limit: number = 10;
+
+  userDetails: any;
+  userRepos: any = [];
+
+  constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+
+    this.apiService.getUser('johnpapa').subscribe((user) => {
+      this.userDetails = user;
+    });
+
+    this.apiService.getReops('johnpapa', 1, 10).subscribe((repos) => {
+      this.userRepos = repos;
+    });
+
+  }
+
+  getUserAndRepo() {
+    this.apiService.getUser(this.username).subscribe((user) => {
+      this.userDetails = user;
+    });
+
+    this.apiService.getReops(this.username, 1, 10).subscribe((repos) => {
+      this.userRepos = repos;
+    });
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -15,5 +15,10 @@ export class ApiService {
     return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  // implement getRepos method by referring to the documentation. Add proper types for the return type and params
+
+  getReops(githubUsername:string,page:number,limit:number){
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos?page=${page}&per_page=${limit}`)
+  }
+
 }
