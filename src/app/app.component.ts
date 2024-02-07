60aa3b0ad433a3e68894f394d6aa2df80d564a36
Deva <devanand151101@gmail.com>
2024-02-07 09:09:06 +0530

    Pagination and Repo lang completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
 
 @Component({
@@ -7,34 +7,95 @@ import { ApiService } from './services/api.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  username: string = '';
+  username: string = 'johnpapa';
   page_no: number = 1;
   limit: number = 10;
+  total_repos: number = 0;
+  total_pages: number = 0;
 
   userDetails: any;
   userRepos: any = [];
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe((user) => {
+    this.apiService.getUser(this.username).subscribe((user: any) => {
       this.userDetails = user;
-    });
-
-    this.apiService.getReops('johnpapa', 1, 10).subscribe((repos) => {
-      this.userRepos = repos;
-    });
-
-  }
-
-  getUserAndRepo() {
-    this.apiService.getUser(this.username).subscribe((user) => {
-      this.userDetails = user;
+      this.total_repos = user.public_repos;
+      this.total_pages = Math.ceil(this.total_repos / this.limit);
     });
 
     this.apiService.getReops(this.username, 1, 10).subscribe((repos) => {
       this.userRepos = repos;
+      this.getRepoLang();
     });
   }
 
+  getUserAndRepo() {
+    this.apiService.getUser(this.username).subscribe((user: any) => {
+      this.userDetails = user;
+      this.total_repos = user.public_repos;
+      this.total_pages = Math.ceil(this.total_repos / this.limit);
+    });
+
+    this.apiService.getReops(this.username, 1, 10).subscribe((repos) => {
+      this.userRepos = repos;
+      this.getRepoLang();
+    });
+  }
+
+  getRepos() {
+    this.apiService
+      .getReops(this.username, this.page_no, this.limit)
+      .subscribe((repos: any) => {
+        this.userRepos = repos;
+        this.cdr.detectChanges();
+        this.getRepoLang();
+      });
+  }
+
+  getRepoLang() {
+    this.userRepos.forEach((repo: any) => {
+      this.apiService
+        .getRepoLangFromUrl(repo.languages_url)
+        .subscribe((lang) => (repo.languages = Object.keys(lang)));
+    });
+  }
+
+  onLimitChange() {
+    this.total_pages = Math.ceil(this.total_repos / this.limit);
+    if (this.page_no > this.total_pages) {
+      this.page_no = this.total_pages;
+    }
+    this.getRepos();
+  }
+
+  nextPage() {
+    if (this.page_no * this.limit < this.total_repos) {
+      this.page_no++;
+      this.getRepos();
+    }
+  }
+
+  prevPage() {
+    if (this.page_no > 1) {
+      this.page_no--;
+      this.getRepos();
+    }
+  }
+
+  getPageNumbers(): number[] {
+    return Array.from({ length: this.total_pages }, (_, index) => index + 1);
+  }
+
+  goToPage(pageNumber: number) {
+    if (
+      pageNumber >= 1 &&
+      pageNumber <= this.total_pages &&
+      pageNumber !== this.page_no
+    ) {
+      this.page_no = pageNumber;
+      this.getRepos();
+    }
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,26 +3,26 @@ import { Injectable } from '@angular/core';
 import { Observable, tap, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  constructor(private httpClient: HttpClient) {}
 
   getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+    return this.httpClient.get(
+      `https://api.github.com/users/${githubUsername}`
+    );
   }
 
   // implement getRepos method by referring to the documentation. Add proper types for the return type and params
 
-  getReops(githubUsername:string,page:number,limit:number){
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos?page=${page}&per_page=${limit}`);
+  getReops(githubUsername: string, page: number, limit: number) {
+    return this.httpClient.get(
+      `https://api.github.com/users/${githubUsername}/repos?page=${page}&per_page=${limit}`
+    );
   }
 
-  getRepoLang(url:string):Observable<any> {
+  getRepoLangFromUrl(url:string){
     return this.httpClient.get(url);
   }
-
 }
